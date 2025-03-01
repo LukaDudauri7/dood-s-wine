939e0ae92fb135d999e1088c4850bfdd3021f141
ldudauri-smartsoft <lukadudauri01@gmail.com>
2025-03-01 13:24:05 +0400

    header partly

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
+        <nav className="App-nav">
           <Link to="/">Home</Link>
           <Link to="/wine">Wine</Link>
           <Link to="/about">About</Link>

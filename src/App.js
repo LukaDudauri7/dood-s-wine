5205748e106585f9becd5ce56ad85fb150f3d686
ldudauri-smartsoft <lukadudauri01@gmail.com>
2025-03-01 12:41:51 +0400

    header components structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/home/Home";
+import Wine from "./components/wine/Wine"
+import About from "./components/about/About"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/wine">Wine</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/wine" element={<Wine />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/components/wine/Wine.jsx
@@ -0,0 +1,5 @@
+function Wine() {
+    return <h2>Wine Page</h2>;
+}
+
+export default Wine;

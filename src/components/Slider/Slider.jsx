98bf9bc2cc0b95aed24005b21f47cb60153f2198
ldudauri-smartsoft <lukadudauri01@gmail.com>
2025-03-13 15:54:37 +0400

    add pagination buttons

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,36 +1,30 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './Slider.css';
 
 const Slider = () => {
   return (
-    <Swiper
-      slidesPerView={1}
-      spaceBetween={10}
-      navigation={{
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }}
-      pagination={{
-        el: '.swiper-pagination',
-        clickable: true,
-      }}
-      loop={true}
-    >
-      <SwiperSlide>
-        <img src="/images/wine1.png" alt="Wine 1" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="/images/wine2.png" alt="Wine 2" />
-      </SwiperSlide>
-      {/* <SwiperSlide>
-        <img src="/images/wine3.png" alt="Wine 3" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="/images/wine4.png" alt="Wine 4" />
-      </SwiperSlide> */}
-    </Swiper>
+    <div className="slider-container">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        slidesPerView={1}
+        spaceBetween={10}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+      >
+        <SwiperSlide>
+          <img src="/images/wine1.png" alt="Wine 1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/wine2.png" alt="Wine 2" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 

3764cdb1e79f53fa0a7b336fba04f686abde70c2
LukaDudauri7 <lukadudauri01@gmail.com>
2025-05-30 11:43:36 +0400

    captions

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,20 +1,17 @@
 import { useNavigate, useLocation } from 'react-router-dom';
+import captionsData from '../../captions.json';
 import './about.css';
 const About = () => {
     const navigate = useNavigate();
     const location = useLocation();
-    const shortText = "Dood's wine  უძველეს ტრადიციებზე დაფუძნებული ღვინის მწარმოებელი კომპანიაა, რომელიც მაღალი ხარისხის პროდუქტის წარმოებაზეა ორიენტირებული.";
-    const fullText = "Dood's wine  უძველეს ტრადიციებზე დაფუძნებული ღვინის მწარმოებელი კომპანიაა, რომელიც მაღალი ხარისხის პროდუქტის წარმოებაზეა ორიენტირებული. ჩვენი მიზანია მომხმარებელს შევთავაზოთ საუკეთესო ხარისხის ღვინო, რომელიც ასახავს ქართული ღვინის კულტურის მდიდარ ისტორიას და ტრადიციებს. დღესდღეობით, კომპანიის ასორტიმენტი ისეთ პრემიუმ ღვინოებს მოიცავს,  როგორიცაა გორული მწვანე, ჩინური, რქაწითელი, ალადასტური და დირბულა. ხოლო რაც შეეხება ღვინის ბოთლის დიზაინსა და ეტიკეტს, მათზეც ჩვენი გუნდი მუშაობს და რეალობაში გადმოაქვს ის იდეები და სურვილები, რასაც მომხმარებლები ისურვებენ.";
     
     const isFullPage = location.pathname === "/About";
     return (
     <div className="about-container">
-      <h2 className="about-title">ჩვენს შესახებ</h2>
-      <p className="about-text">{isFullPage ? fullText : shortText}</p>
+      <h2 className="about-title">{captionsData.about.title}</h2>
+      <p className="about-text">{isFullPage ? captionsData.about.fullText : captionsData.about.shortText}</p>
        {!isFullPage && (
-        <button className="toggle-button" onClick={() => navigate("/About")}>
-          ვრცლად
-        </button>
+        <button className="toggle-button" onClick={() => navigate("/About")}>{captionsData.about.seeMore}</button>
       )}
     </div>
   );

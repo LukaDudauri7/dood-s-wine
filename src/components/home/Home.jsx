fa1d17b80ace37ed4efaba304e50f981108b86ed
LukaDudauri7 <lukadudauri01@gmail.com>
2025-06-03 17:03:44 +0400

    .

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -30,7 +30,14 @@ const Home = () => {
             </motion.h2>
 
             <SocialBar />
-            <Slider />
+
+            <motion.div
+                initial={{ opacity: 0, scale: 0.95 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 1, delay: 0.9 }}
+            >
+                <Slider />
+            </motion.div>
         </div>
     );
 };
